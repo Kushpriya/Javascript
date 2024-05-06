a4161c628f7fe3b4ee385264c608ed1bbd6bd506
Priya Kushawaha <priyakushawaha73@gmail.com>
2024-05-06 17:08:28 +0545

    js

---
+++ Functions & Methods/script.js
@@ -0,0 +1,77 @@
+/*function myFunction(){
+    console.log("hello world!!");
+    console.log("Priya Kushwaha");
+}
+myFunction();
+*/
+
+/*
+function myFunction(msg){
+    //parameter ->input
+    console.log(msg);
+}
+myFunction(" I Love JS "); //argument
+*/
+
+/*
+function sum(x,y){
+    s=x+y;
+    return s;
+}
+let val=sum(3,4);
+console.log(val);
+//  function parameter -> they are like local variables of function -> block scope
+*/
+
+/*
+//arrow function
+const sum=(a,b)=>{
+    console.log(a+b);
+}
+
+const mul = (a,b) =>{
+    console.log(a*b);
+}
+
+const print=()=>{
+    console.log("hello world");
+}
+*/
+
+/*
+function countVowels = (str) => {
+    let count = 0;
+    for (const char of str )
+        {
+            if(char === "a" ||
+             char === "e"||
+             char === "i"||
+             char === "o"||
+             char === "u")
+                {
+                count++;
+            }
+        }
+        return count;
+}
+*/
+/*
+let arr = [1,2,3,4,5,8,6];
+arr.forEach(function printVal(val) {
+    console.log(val);
+}
+);
+*/
+/*
+// let arr = [1,2,5,8,57,9];
+let arr = ["priya","sita","gita","ram","shyam"];
+arr.forEach((val)=>{
+    console.log(val.toUpperCase());
+});
+*/
+/*
+let arr = [1,2,3,4,5];
+arr.forEach((num)=>{
+ console.log(num*num); //square
+});
+*/
